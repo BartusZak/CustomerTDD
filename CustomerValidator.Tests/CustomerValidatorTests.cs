75243552b2bff630c7c6b5addf2fe0b7da946917
BartusZak <b.ploszynski@gmail.com>
2018-03-11 20:14:35 +0100

    Dodaje nowy projekt i referencje.

---
+++ CustomerValidator.Tests/CustomerValidatorTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace CustomerValidator.Tests
+{
+    public class CustomerValidatorTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
