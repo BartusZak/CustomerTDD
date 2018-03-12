9be0a7c3ae105a30f6ae28c93e916b0c24223cf3
BartusZak <b.ploszynski@gmail.com>
2018-03-12 07:54:29 +0100

    Tworzymy mocka ręcznie.

--- CustomerTDD/CustomerValidator.cs
+++ CustomerTDD/CustomerValidator.cs
@@ -9,5 +9,17 @@ namespace CustomerTDD
             if (customer == null) throw new ArgumentNullException(nameof(customer));
             throw new NotImplementedException();
         }
+
+        internal class CustomerMock : ICustomer
+        {
+            private readonly int _expectedAge;
+
+            public CustomerMock(int expectedAge)
+            {
+                _expectedAge = expectedAge;
+            }
+
+            public int GetAge() => _expectedAge;
+        }
     }
 }
